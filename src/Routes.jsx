a7950a46cd539c8d235378fd61bbbcd45e30316d
evangelina <evangelina.gomes@ellecktra.com>
2020-12-18 11:39:24 -0300

    fixes in folder structure

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import BoilerTypes from './components/BoilerTypes/BoilerTypes';
-import TopBar from "./components/share/topBar/TopBar";
-import LeftBar from "./components/share/leftBar/LeftBar";
-import MainContainer from "./components/share/MainContainer/MainContainer";
-import styles from  "./Routes.module.css";
+import BoilerTypes from './components/BoilerTypes';
+import TopBar from "./components/Shared/topBar";
+import LeftBar from "./components/Shared/leftBar";
+import MainContainer from "./components/Shared/MainContainer";
+
 
 const wrapWithNav = (component) => (
-  <div className={styles.Main}>
-    <TopBar className={styles.TopBar}/>
-    <div className={styles.Content}>
-      <LeftBar className={styles.LeftBar}/>
-      <MainContainer className={styles.MainContainer} container={component}></MainContainer>
+  <div>
+    <TopBar/>
+    <div>
+      <LeftBar />
+      <MainContainer container={component}></MainContainer>
     </div>
   </div>
 );
@@ -19,8 +19,8 @@ const wrapWithNav = (component) => (
 const Routes = () => {
   return (
     <Switch>
-      <Redirect exact push from="/" to="/companies" />
-      <Route path="/companies" >
+      <Redirect exact push from="/" to="/boilerTypes" />
+      <Route path="/boilertypes" >
         {wrapWithNav(<BoilerTypes />)}
       </Route>
     </Switch>
--- src/components/Shared/leftBar/index.jsx
+++ src/components/Shared/leftBar/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NavBar from '../navBar/NavBar';
+import NavBar from '../navBar';
 
 class LeftBar extends Component {
   render() {
