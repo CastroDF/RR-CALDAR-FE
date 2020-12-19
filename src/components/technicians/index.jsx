9c72e34a38261a4dbd05f9677106f73bd21f3664
Nicolas Mandrile <mandrilenico@gmail.com>
2020-12-19 17:43:56 -0300

    Method get added and display the data

---
+++ src/components/technicians/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import HeaderList from './headerList.jsx';
+import Techs from './Technicians';
+import Addbutton from './addbutton.jsx';
+import styles from './index.module.css';
+
+function Technicians () {
+  useEffect(() => {
+    fechItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+  const fechItems = async () => {
+    const data = await fetch('https://rrcaldar.herokuapp.com/technicians');
+    const items = await data.json();
+    console.log(items);
+    setItems(items);
+  };
+  return (
+    <div className={styles.mainColumn}>
+      <HeaderList />
+      {items.map((item) => (
+        <Techs
+          key={item.id}
+          item={item}
+          setItems={setItems} />
+      ))}
+      <Addbutton />
+    </div>
+  );
+}
+
+export default Technicians;
