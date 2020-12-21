0c2b268c23852c4825cd0d46c7992540e3c914e8
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-12-21 18:42:46 -0300

    refresh

--- src/components/PageNotFound/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PageNotFound = () => (
-  <section className="PageNotFound">
-    <h3> Sorry ! Page Not Found :( </h3>
-  </section>
-)
-
-export default PageNotFound
