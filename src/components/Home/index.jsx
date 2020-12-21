ba300557acc5e10611e6c102003b80fa8f69da9f
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-12-21 19:54:43 -0300

    Add Routes Customers & Home

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.module.css';
+
+const Customers = () => (
+  <section>
+    <h3>Welcome CaldAR Web</h3>
+  </section>
+);
+
+export default Customers;
--- src/components/Shared/TopBar/index.jsx
+++ src/components/Shared/TopBar/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const TopBar = () => (
   <section className={style.container}>
-    <h1 className={style.header}>Caldar</h1>
+    <h1 className={style.header}>CaldAR</h1>
     <Link className={style.logOut} to="/login">Log out</Link>
   </section>
 );
