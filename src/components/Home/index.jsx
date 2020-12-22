b6c750583d35b2fbac2470d137dbfeda90f6e94b
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-12-22 00:10:57 -0300

    adding 'to'

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,7 @@ import style from './Home.module.css';
 
 const Customers = () => (
   <section>
-    <h3 className={style.homeContainer}>Welcome CaldAR Web</h3>
+    <h3 className={style.homeContainer}>Welcome to CaldAR Web</h3>
   </section>
 );
 
